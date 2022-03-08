6217fa18ebc29e71b8eaa3e785ca7321836fea86
jccastro <jc.castro2636@uniandes.edu.co>
2022-03-07 23:41:25 -0500

    Monster rolodex built with class components

---
+++ src/components/search-box/search-box.component.jsx
@@ -0,0 +1,18 @@
+import { Component } from "react";
+import "./search-box.styles.css";
+
+class SearchBox extends Component {
+  render() {
+    const { placeholder, onSearchHandler, className } = this.props;
+    return (
+      <input
+        className={className}
+        type="search"
+        placeholder={placeholder}
+        onChange={onSearchHandler}
+      />
+    );
+  }
+}
+
+export default SearchBox;
